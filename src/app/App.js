5bcbe22a8eab35fc1ca905f67f9008af5717ea7b
Denzel Uriarte <denzeluriartem@gmail.com>
2023-04-06 10:43:45 -0700

    added more structure

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/zounter/Counter';
+import logo from '../logo.svg';
+import { Counter } from '../features/zounter/Counter';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -54,5 +54,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
