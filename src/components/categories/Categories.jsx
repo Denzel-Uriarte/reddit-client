3cfd8f6d827a7c9937a3dd53adfc8be237b2f152
Denzel Uriarte <denzeluriartem@gmail.com>
2023-05-29 17:14:02 -0700

    fixed small error

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '../button/Button';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 export const Categories = () => {
   return (
